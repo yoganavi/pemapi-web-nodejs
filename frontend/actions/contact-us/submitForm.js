6afc03b801a677a492e646cc9c86acbf43896449
yoganavi <riandgrown@gmail.com>
2025-08-27 14:39:31 +0700

    change suubmitFomr to direct send to googlesheet because in server the encore backend not yet deploy

--- frontend/actions/contact-us/submitForm.js
+++ frontend/actions/contact-us/submitForm.js
@@ -6,9 +6,9 @@ export default async function submitForm(req,res){
   if(body.nama.trim() == '' || body.pesan.trim().length < 10 || body.phone.length < 11){
     req.flash('alert', 'message not send');
   }else{
-    // let sending = await axios.post('https://script.google.com/macros/s/AKfycbw4lpVVgxYmsEOKamdTgPa38gPs76Gg-gAh8ihTCiDRvOsr-0fCD3mIbpOXfqlf9KJH/exec', body);
-    let sending = await axios.post('http://127.0.0.1:4000/pemapi-web-nodejs/contact-us/submit', {body});
-    req.flash('alert', sending.data.message);
+    let sending = await axios.post('https://script.google.com/macros/s/AKfycbw4lpVVgxYmsEOKamdTgPa38gPs76Gg-gAh8ihTCiDRvOsr-0fCD3mIbpOXfqlf9KJH/exec', body);
+    // let sending = await axios.post('http://127.0.0.1:4000/pemapi-web-nodejs/contact-us/submit', {body});
+    req.flash('alert', sending.data.result);
   }
   res.redirect('/contact-us');  
 };
